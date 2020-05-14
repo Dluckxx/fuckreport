98a9e17398a2ac3c51a4f5d6376a27a91eec0a05
Dluck <dutiesheng@outlook.com>
2020-05-14 12:13:08 +0800

    Initial commit

---
+++ src/main/java/dluck/fuckreport/FuckreportApplication.java
@@ -0,0 +1,13 @@
+package dluck.fuckreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dluck/fuckreport/FuckreportApplicationTests.java
@@ -0,0 +1,13 @@
+package dluck.fuckreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
